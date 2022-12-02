dad75cc3fee61610ebaaa82ee45edc3ddfb53778
Milan Hauth <milahu@gmail.com>
2022-12-02 21:22:29 +0100

    fix

--- src/readline.js
+++ src/readline.js
@@ -5,11 +5,16 @@
 * or less than one line, or nothing.
 */
 export async function readline(stdin) {
-  let buf = "";
+  /** @type {string | undefined} */
+  let buf;
   while (true) {
     const chunk = stdin.next();
+    if (chunk.done || chunk.value == undefined) break;
+    if (buf == undefined) buf = ""
     buf += chunk.value;
-    if (chunk.done || chunk.value.endsWith("\n")) break;
+    //console.dir({chunk});
+    //throw new Error("todo")
+    if (chunk.value.endsWith("\n")) break;
   }
   return buf;
 }
