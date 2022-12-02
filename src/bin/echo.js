301aba8229b2fa78791c48643485e9ee7f3468d7
Milan Hauth <milahu@gmail.com>
2022-12-03 00:10:23 +0100

    bin.echo: return 0

--- src/bin/echo.js
+++ src/bin/echo.js
@@ -2,5 +2,6 @@ export function echo(args = [], options = {}) {
   /** @return {AsyncGenerator<[number, string]>} */
   return async function* echo_() {
     yield [1, args.join(" ") + "\n"]
+    return 0
   }
 }
--- src/bin/echo.test.js
+++ src/bin/echo.test.js
@@ -2,7 +2,13 @@ import "./.test.js"
 import { echo } from "./.bin.js"
 //import { stringify } from "./.lib.js"
 ;(async function main() {
-  for await (const [stream, chunk] of echo(["hello", "world"])()) {
+  var it = echo(["hello", "world"])()
+  let result = await it.next()
+  while (result.done == false) {
+    const [stream, chunk] = result.value
     console.log(`stream ${stream}: chunk: ` + JSON.stringify(chunk))
+    result = await it.next()
   }
+  const code = result.value
+  console.log(`code ${code}`)
 })()
